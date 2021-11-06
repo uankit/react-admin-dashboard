a75b19e6738959aab681c8e35d327f4092e35ddf
Ankit <uankit53@outlook.com>
2021-11-06 20:57:21 +0530

    Created Chart component

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 5000,
+    },
+    {
+      name: "Mar",
+      "Active User": 2000,
+    },
+    {
+      name: "Apr",
+      "Active User": 6000,
+    },
+    {
+      name: "May",
+      "Active User": 7000,
+    },
+    {
+      name: "June",
+      "Active User": 8000,
+    },
+    {
+      name: "July",
+      "Active User": 4000,
+    },
+    {
+      name: "Aug",
+      "Active User": 3000,
+    },
+    {
+      name: "Sept",
+      "Active User": 1000,
+    },
+    {
+      name: "Oct",
+      "Active User": 2000,
+    },
+    {
+      name: "Nov",
+      "Active User": 5000,
+    },
+    {
+      name: "Dec",
+      "Active User": 2000,
+    },
+  ];
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,13 @@
+import Chart from "../../components/charts/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./homepage.css";
+import { data } from "../../data";
 
 function HomePage() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={data} title="User Analytics" grid dataKey="Active User" />
     </div>
   );
 }
